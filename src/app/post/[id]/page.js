cfe6407fd7a0af94ab82b6d2ab708a36db099b7f
aidin-rezaei <Aydinrezaeefdr@gmail.com>
2023-03-17 10:33:55 -0500

    update page.js

--- src/app/post/[id]/page.js
+++ src/app/post/[id]/page.js
@@ -1,7 +1,11 @@
+import { useRouter } from "next/navigation";
+
 export default function Post( ) {
+  const router = useRouter()
+  const { pid } = router.query
     return (
       <div className='bg-gray-800 h-screen p-16 text-gray-100'>
-hyjk
+hyjk {pid}
       </div>
     );
   }
