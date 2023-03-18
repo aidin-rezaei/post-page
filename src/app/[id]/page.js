df2e43dc8508d8b325d9f48b0f1598e906bc9bc8
aidin-rezaei <Aydinrezaeefdr@gmail.com>
2023-03-18 18:15:38 -0500

    update page.js

--- src/app/[id]/page.js
+++ src/app/[id]/page.js
@@ -1,26 +1,83 @@
+import Card from '@/components/Card'
+import Content from '@/components/Content'
+import Link from 'next/link';
+// import styles from './page.module.css'
 
-export const revalidate = 10;
+async function getData() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
+    return data;
+}
+async function paginationNext(conut, lastpage) {
+    const element = [];
+    for (let index = conut + 1; index <= lastpage; index++) {
+        if (conut + 2 >= index) {
+            element.push(index)
+        }
+    }
+    return element;
+}
+async function paginationPrevious(conut) {
+    const element = [];
+    for (let index = conut - 1; index >= 1; index--) {
+        if (conut - 2 <= index) {
+            element.push(index)
+        }
+    }
+    return element.sort();
+}
 
-// const getCard = async (cardId) => {
-//   const pb = new PocketBase('http://127.0.0.1:8090');
+export default async function Nextpost({ params }) {
+    const date = await getData(),
+        conut = Number(params.id),
+        lastpage = date[date.length - 1].userId,
+        getpaginationNext = await paginationNext(conut, lastpage),
+        getpaginationPrevious = await paginationPrevious(conut)
 
-//   return await pb.collection('cards').getOne(cardId);
-// }
+    return (
+        <Content>
+            {date.map((item) => {
+                return (
+                    <>
+                        {
+                            conut === item.userId ?
+                                <div key={item.id} >
+                                    <Card item={item} key={item.id} />
+                                </div>
+                                :
+                                ""
+                        }
+                    </>
+                )
+            })}
+            <div className='pagination w-[100%] flex justify-center'>
+                <Link href={`/1`} className="hover:text-red-500 mx-1 my-5 text-white">
+                    <p className='bg-blue-400 px-2 py-1'>1</p>
+                </Link>
+                <p className='px-2 py-1 my-5'>...</p>
+                {getpaginationPrevious.map((item) => {
+                    console.log(item);
+                    return (
+                        <Link href={`/${item}`} key={item} className="hover:text-red-500 mx-1 my-5 text-white">
+                            <p className='bg-blue-400 px-2 py-1'>{item}</p>
+                        </Link>
+                    )
+                })}
+                <p className='px-2 py-1 my-5'>{conut}</p>
 
-
-const Page = async ({ params }) => {
-  // const card = await getCard(params.id);
-// console.log(params);
-  return (
-    <div>
-      ff
-      {/* <h1>{card.title}</h1>
-      <div className={styles.card}>
-        <h5>{card.content}</h5>
-        <p>{card.created}</p>
-      </div> */}
-    </div>
-  )
-};
-
-export default Page;
+                {getpaginationNext.map((item) => {
+                    console.log(item);
+                    return (
+                        <Link href={`/${item}`} key={item} className="hover:text-red-500 mx-1 my-5 text-white">
+                            <p className='bg-blue-400 px-2 py-1'>{item}</p>
+                        </Link>
+                    )
+                })}
+                <p className='px-2 py-1 my-5'>...</p>
+                <Link href={`/${lastpage}`} className="hover:text-red-500 mx-1 my-5 text-white">
+                    <p className='bg-blue-400 px-2 py-1'>{lastpage}</p>
+                </Link>
+            </div>
+        </Content >
+    )
+}
