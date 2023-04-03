651d0d092152286ffda8dcc6c2e2c0ba37dbe53d
aidin-rezaei <Aydinrezaeefdr@gmail.com>
2023-04-03 06:54:00 -0500

    update src/components/Sidebar/index.jsx

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
                 <p className="text-gray-400 py-[30px]">
                     Get awesome content delivered straight to your inbox.
                 </p>
-                <input className="py-[5px] bg-gray-400" type="text" name="mail" id="mail" />
+                <input className="p-[8px] bg-gray-600 w-[100%]" placeholder="Enter your email address" type="text" name="mail" id="mail" />
             </div>
         </div>
     );
